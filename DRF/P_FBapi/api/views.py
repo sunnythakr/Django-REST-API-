72871a9d0a9c8624ba8c104c8ed61d6ef24dacce
sunny <sunnysacademy0@gmail.com>
2023-10-15 17:44:25 +0530

    DRF : function based API

--- DRF/P_FBapi/api/views.py
+++ DRF/P_FBapi/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+import requests
 
 # Create your views here.
 
@@ -18,3 +21,22 @@ def hello_world(request):
     if request.method == "POST":
         print(request.data)
         return Response({'msg': 'This is Post Request'})
+
+@api_view(['GET','POST','PUT', "DELETE"])
+def student_api(request):
+    if request.method == 'GET':
+        id = request.data.get('id')
+        if id is not None:
+            stu = Student.objects.get(id = id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many = True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Created'})
+        return Response(serializer.errors)
+ 
--- DRF/P_FBapi/fbapiapp.py
+++ DRF/P_FBapi/fbapiapp.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-URL = "http://127.0.0.1:8000/fbapi/"
-data = {
-'name':'Vaidik',
-'roll':101,
-'city': 'jaipur'}
+URL = "http://127.0.0.1:8000/studentapi/"
+# data = {
+# 'name':'Vaidik',
+# 'roll':101,
+# 'city': 'jaipur'}
 def get_data(id = None):
     data = {}
     if id is not None:
@@ -15,7 +15,7 @@ def get_data(id = None):
     data = r.json()
     print(data)
 
-get_data()
+# get_data()
 
 def post_data():
     data = {
@@ -25,6 +25,10 @@ def post_data():
     headers = {'content-type':'application/json'}
     json_data = json.dumps(data)
     r = requests.post(url=URL,headers = headers, data=json_data)
+    if r.status_code == 201:  # Assuming HTTP 201 Created is returned for a successful POST
+        print("Data successfully posted.")
+    else:
+        print("Failed to post data. Status code:", r.status_code)
     data = r.json()
     print(data)
 
